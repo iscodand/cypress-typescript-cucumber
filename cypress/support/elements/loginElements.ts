50700b1d759faa40c6d3e4ec5569926da80ba3c9
iscodand <iscodand@outlook.com>
2023-02-23 17:50:59 -0300

    add: more login elements

--- cypress/support/elements/loginElements.ts
+++ cypress/support/elements/loginElements.ts
@@ -2,6 +2,8 @@ class LoginElements {
 
     accessLogin = () => { return ('[show-authed="false"] > :nth-child(2) > .nav-link'); };
 
+    needAnAccountButton = () => { return ('p.text-xs-center > [ui-sref="app.register"]'); };
+
     emailField = () => { return (':nth-child(2) > .form-control'); };
     passwordField = () => { return (':nth-child(3) > .form-control'); };
     loginButton = () => { return ('.btn'); };
