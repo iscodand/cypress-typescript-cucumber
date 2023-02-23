0f9461be5cbfae78e688c395c664badb21bc67dc
iscodand <iscodand@outlook.com>
2023-02-23 17:51:19 -0300

    add: steps definitions for login

--- cypress/support/steps/loginSteps.ts
+++ cypress/support/steps/loginSteps.ts
@@ -2,6 +2,7 @@
 
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import loginPage from "../page/loginPage";
+import commands from "../../utils/cypressCommands";
 
 
 Given("acesso ao site", () => {
@@ -20,6 +21,14 @@ When("clicar no botão entrar", () => {
     loginPage.clickLogin();
 });
 
-Then("deve ser apresentado o username {}", (username: string) => {
-    loginPage.verifyUsernameExists(username);
+When("clicar no botão 'Need an Account?'", () => {
+    loginPage.clickNeedAnAccount();
+});
+
+Then("deve ser apresentado o texto {}", (text: string) => {
+    commands.verifyTextExists(text);
+});
+
+Then("deve ser apresentada uma mensagem de erro {}", (errorMessage: string) => {
+    loginPage.verifyErrorMessage(errorMessage);
 });
