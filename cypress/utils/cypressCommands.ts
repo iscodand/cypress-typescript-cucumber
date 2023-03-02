24e5a15d95a9c7cbc6d298735cd281d0333d0f22
iscodand <iscodand@outlook.com>
2023-03-02 16:58:42 -0300

    env: login with secret variables

--- cypress/utils/cypressCommands.ts
+++ cypress/utils/cypressCommands.ts
@@ -3,49 +3,56 @@
 
 class CypressCommands {
 
-    login() {
+    login(): void {
+        const email: string = Cypress.env('email');
+        const password: string = Cypress.env('password');
+
         let request = {
             "user": {
-                "email": "usuario@deteste.com",
-                "password": "123456",
+                "email": email,
+                "password": password,
             }
         };
 
         cy.request('POST',
-                   'https://api.realworld.io/api/users/login',
-                   request)
-                   .then((response) => {
-                   window.localStorage.setItem("jwtToken", response.body.user.token);
-        });
+            'https://api.realworld.io/api/users/login',
+            request)
+            .then((response) => {
+                window.localStorage.setItem("jwtToken", response.body.user.token);
+            });
     };
 
-    visit(url: string) : void {
+    visit(url: string): void {
         cy.visit(url);
     };
 
-    cleanVisit(url: string) : void {
+    cleanVisit(url: string): void {
         cy.clearAllCookies();
         cy.clearAllLocalStorage();
         cy.visit(url);
     };
 
-    fillField(field: string, text: string) : void {
+    fillField(field: string, text: string): void {
         cy.get(field).type(text);
     };
 
-    clickButton(button: string) : void {
+    clickButton(button: string): void {
         cy.get(button).click();
     };
 
-    verifyTextExists(text: string) : void {
+    verifyTextExists(text: string): void {
         cy.contains(text);
     };
 
-    clickIfTextExists(text: string) : void {
+    textShouldNotBeVisible(text: string): void {
+        cy.contains(text).should('not.exist');
+    };
+
+    clickIfTextExists(text: string): void {
         cy.contains(text).click();
     };
 
-    clearTextArea(field: string) : void {
+    clearTextArea(field: string): void {
         cy.get(field).clear();
     };
 
