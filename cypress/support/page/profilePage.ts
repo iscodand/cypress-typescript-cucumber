d251a8697fb3838d2b97f816ca55d64c8c6c57dd
iscodand <iscodand@outlook.com>
2023-03-02 16:57:52 -0300

    add: commands and refactor code style

--- cypress/support/page/profilePage.ts
+++ cypress/support/page/profilePage.ts
@@ -1,5 +1,3 @@
-// @ts-check
-
 import profileElements from "../elements/profileElements";
 import commands from "../../utils/cypressCommands";
 import mockData from "../../utils/faker";
@@ -12,33 +10,46 @@ class ProfilePage {
     public bio: string = mockData.generateRandomBio();
     public username: string = mockData.generateRandomUsername();
 
-    visitProfileUrl() {
+    visitProfileUrl(): void {
         commands.visit(PROFILE_URL);
     };
 
-    changeBioContent() {
+    changeBioContent(): void {
         commands.clearTextArea(profileElements.changeBioField());
         commands.fillField(profileElements.changeBioField(), this.bio);
     };
 
-    changeUsername(username: string = this.username) {
+    changeUsername(username: string = this.username): void {
         commands.clearTextArea(profileElements.changeUsernameField());
         commands.fillField(profileElements.changeUsernameField(), username);
     };
 
-    clickUpdateSettings() {
+    changeEmailInvalid(email: string = 'user@email.com'): void {
+        commands.clearTextArea(profileElements.changeEmailField());
+        commands.fillField(profileElements.changeEmailField(), email);
+    }
+
+    clickUpdateSettings(): void {
         commands.clickIfTextExists('Update Settings');
     };
 
-    verifyBioIsUpdated() {
+    clickButtonLogout(): void {
+        commands.clickIfTextExists('Or click here to logout.')
+    };
+
+    verifyBioIsUpdated(): void {
         commands.verifyTextExists(this.bio);
     };
 
-    verifyUsernameIsUpdated() {
+    verifyUsernameIsUpdated(): void {
         commands.verifyTextExists(this.username);
+    };
+
+    verifyLogoutSuccessful(): void {
+        commands.textShouldNotBeVisible(this.username);
     };
 
 };
 
-const profilePage = new ProfilePage();
+const profilePage: ProfilePage = new ProfilePage();
 export default profilePage;
