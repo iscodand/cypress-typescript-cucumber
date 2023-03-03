15813abbbe34760d7775627e09717a9d3eec75df
iscodand <iscodand@outlook.com>
2023-03-03 17:03:12 -0300

    trying to fix: github actionscls

--- cypress/utils/cypressCommands.ts
+++ cypress/utils/cypressCommands.ts
@@ -10,7 +10,7 @@ class CypressCommands {
         let request = {
             "user": {
                 "email": email,
-                "password": password,
+                "password": `"${password}"`,
             }
         };
 
